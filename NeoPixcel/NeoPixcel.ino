41ba05da296946310ef78b5d4b5b95fb7aaf3f74
WaveThanaphon <thanaphon.wich@lic.ac.th>
2024-11-08 12:07:09 +0700

    Update NeoPixcel.ino

--- NeoPixcel/NeoPixcel.ino
+++ NeoPixcel/NeoPixcel.ino
@@ -1,5 +1,5 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN            0 // กำหนดขาที่ต่อกับ NeoPixel
+#define PIN            2 // กำหนดขาที่ต่อกับ NeoPixel
 #define NUMPIXELS      12 // กำหนดจำนวน LED ที่ต่อกับ NeoPixel
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // สร้างอ็อบเจ็ค pixels
 void setup() 
@@ -7,26 +7,30 @@ void setup()
   pixels.begin(); // สั่งให้เริ่มต้นการทำงาน
   pixels.setBrightness(10); // สั่งให้ความสว่างเป็น 10%
 }
+
+int currentColor = 0;
+
 void loop() 
 {
-   for(int i=0;i<NUMPIXELS;i++) // วนลูปเปลี่ยนสีของ LED
-   {
-      pixels.setPixelColor(i, pixels.Color(255,0,0));   // แสดงสีแดง
-      pixels.show();    // สั่งให้แสดงสีที่เลือก
+   for(int i=0; i<NUMPIXELS; i++){
+         if (currentColor == 0) 
+         {
+            pixels.setPixelColor(i, pixels.Color(255,0,0)); // Red
+         } 
+         else if (currentColor == 1) 
+         {
+            pixels.setPixelColor(i, pixels.Color(0,255,0)); // Green
+         } 
+         else if (currentColor == 2) 
+         {
+            pixels.setPixelColor(i, pixels.Color(0,0,255)); // Blue
+         }
    }
-   delay(1000);  
-
-   for(int i=0;i<NUMPIXELS;i++)
+   pixels.show();
+   delay(1000);
+   currentColor++;
+   if (currentColor > 2) 
    {
-    pixels.setPixelColor(i, pixels.Color(0,255,0));   // Moderately bright green color.
-    pixels.show();    // This sends the updated pixel color to the hardware.
+      currentColor = 0;
    }
-   delay(1000);  // Delay for a period of time (in milliseconds).
-   
-   for(int i=0;i<NUMPIXELS;i++)
-   {
-    pixels.setPixelColor(i, pixels.Color(0,0,255));   // Moderately bright green color.
-    pixels.show();    // This sends the updated pixel color to the hardware.
-   }
-   delay(1000);  // Delay for a period of time (in milliseconds).
 }
