26d6e586ae187aa3d34a34907b0c7d7b17af917a
Nathee Srina <thanaphon.wich@lic.ac.th>
2024-11-08 11:42:20 +0700

    ๊ยกฟะำ

--- NeoPixcel/NeoPixcel.ino
+++ NeoPixcel/NeoPixcel.ino
@@ -7,28 +7,62 @@ void setup()
   pixels.begin(); // สั่งให้เริ่มต้นการทำงาน
   pixels.setBrightness(10); // สั่งให้ความสว่างเป็น 10%
 }
+// void loop() 
+// {
+//    for(int i=0;i<NUMPIXELS;i++) // วนลูปเปลี่ยนสีของ LED
+//    {
+//       pixels.setPixelColor(i, pixels.Color(255,0,0));   // แสดงสีแดง
+//       pixels.show();    // สั่งให้แสดงสีที่เลือก
+//    }
+//    delay(1000);  
+
+//    for(int i=0;i<NUMPIXELS;i++)
+//    {
+//     pixels.setPixelColor(i, pixels.Color(0,255,0));   // Moderately bright green color.
+//     pixels.show();    // This sends the updated pixel color to the hardware.
+//    }
+//    delay(1000);  // Delay for a period of time (in milliseconds).
+   
+//    for(int i=0;i<NUMPIXELS;i++)
+//    {
+//     pixels.setPixelColor(i, pixels.Color(0,0,255));   // Moderately bright green color.
+//     pixels.show();    // This sends the updated pixel color to the hardware.
+//    }
+//    delay(1000);  // Delay for a period of time (in milliseconds).
+// }
+unsigned long previousMillis = 0;
+const long interval = 1000;
+int currentColor = 0;
+
 void loop() 
 {
-   unsigned long previousMillis = 0;
-   const long interval = 1000;
-   int colorIndex = 0;
-   uint32_t colors[] = {pixels.Color(255, 0, 0), pixels.Color(0, 255, 0), pixels.Color(0, 0, 255)};
+   unsigned long currentMillis = millis();
 
-   while (true) {
-      unsigned long currentMillis = millis();
+   if (currentMillis - previousMillis >= interval) 
+   {
+      previousMillis = currentMillis;
 
-      if (currentMillis - previousMillis >= interval) {
-         previousMillis = currentMillis;
-
-         for (int i = 0; i < NUMPIXELS; i++) {
-            pixels.setPixelColor(i, colors[colorIndex]);
+      for(int i=0; i<NUMPIXELS; i++) 
+      {
+         if (currentColor == 0) 
+         {
+            pixels.setPixelColor(i, pixels.Color(255,0,0)); // Red
+         } 
+         else if (currentColor == 1) 
+         {
+            pixels.setPixelColor(i, pixels.Color(0,255,0)); // Green
+         } 
+         else if (currentColor == 2) 
+         {
+            pixels.setPixelColor(i, pixels.Color(0,0,255)); // Blue
          }
-         pixels.show();
+      }
+      pixels.show();
 
-         colorIndex++;
-         if (colorIndex >= 3) {
-            colorIndex = 0;
-         }
+      currentColor++;
+      if (currentColor > 2) 
+      {
+         currentColor = 0;
       }
    }
 }
