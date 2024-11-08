14e54c0c783347899352a54cf04ed23b6dc20ef8
WaveThanaphon <thanaphon.wich@lic.ac.th>
2024-11-08 12:08:42 +0700

    Update NeoPixcel.ino

--- NeoPixcel/NeoPixcel.ino
+++ NeoPixcel/NeoPixcel.ino
@@ -1,36 +1,28 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN            2 // กำหนดขาที่ต่อกับ NeoPixel
-#define NUMPIXELS      12 // กำหนดจำนวน LED ที่ต่อกับ NeoPixel
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // สร้างอ็อบเจ็ค pixels
-void setup() 
-{
-  pixels.begin(); // สั่งให้เริ่มต้นการทำงาน
-  pixels.setBrightness(10); // สั่งให้ความสว่างเป็น 10%
+#define PIN 2                                                                        // กำหนดขาที่ต่อกับ NeoPixel
+#define NUMPIXELS 12                                                                 // กำหนดจำนวน LED ที่ต่อกับ NeoPixel
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);  // สร้างอ็อบเจ็ค pixels
+void setup() {
+  pixels.begin();            // สั่งให้เริ่มต้นการทำงาน
+  pixels.setBrightness(10);  // สั่งให้ความสว่างเป็น 10%
 }
 
 int currentColor = 0;
 
-void loop() 
-{
-   for(int i=0; i<NUMPIXELS; i++){
-         if (currentColor == 0) 
-         {
-            pixels.setPixelColor(i, pixels.Color(255,0,0)); // Red
-         } 
-         else if (currentColor == 1) 
-         {
-            pixels.setPixelColor(i, pixels.Color(0,255,0)); // Green
-         } 
-         else if (currentColor == 2) 
-         {
-            pixels.setPixelColor(i, pixels.Color(0,0,255)); // Blue
-         }
-   }
-   pixels.show();
-   delay(1000);
-   currentColor++;
-   if (currentColor > 2) 
-   {
-      currentColor = 0;
-   }
+void loop() {
+  for (int i = 0; i < NUMPIXELS; i++) {
+    if (currentColor == 0) {
+      pixels.setPixelColor(i, pixels.Color(255, 0, 0));  // Red
+    } else if (currentColor == 1) {
+      pixels.setPixelColor(i, pixels.Color(0, 255, 0));  // Green
+    } else if (currentColor == 2) {
+      pixels.setPixelColor(i, pixels.Color(0, 0, 255));  // Blue
+    }
+  }
+  pixels.show();
+  delay(1000);
+  currentColor++;
+  if (currentColor > 2) {
+    currentColor = 0;
+  }
 }
