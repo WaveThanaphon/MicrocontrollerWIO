e8e53466c478e980035efcad50cf7097ddfbbf52
Nathee Srina <thanaphon.wich@lic.ac.th>
2024-11-08 11:57:50 +0700

    Update NeoPixcel.ino

--- NeoPixcel/NeoPixcel.ino
+++ NeoPixcel/NeoPixcel.ino
@@ -7,30 +7,26 @@ void setup()
   pixels.begin(); // สั่งให้เริ่มต้นการทำงาน
   pixels.setBrightness(10); // สั่งให้ความสว่างเป็น 10%
 }
-
-int currentColor = 0;
-
 void loop() 
 {
-   for(int i=0; i<NUMPIXELS; i++){
-         if (currentColor == 0) 
-         {
-            pixels.setPixelColor(i, pixels.Color(255,0,0)); // Red
-         } 
-         else if (currentColor == 1) 
-         {
-            pixels.setPixelColor(i, pixels.Color(0,255,0)); // Green
-         } 
-         else if (currentColor == 2) 
-         {
-            pixels.setPixelColor(i, pixels.Color(0,0,255)); // Blue
-         }
-   }
-   pixels.show();
-   delay(1000);
-   currentColor++;
-   if (currentColor > 2) 
+   for(int i=0;i<NUMPIXELS;i++) // วนลูปเปลี่ยนสีของ LED
    {
-      currentColor = 0;
+      pixels.setPixelColor(i, pixels.Color(255,0,0));   // แสดงสีแดง
+      pixels.show();    // สั่งให้แสดงสีที่เลือก
    }
+   delay(1000);  
+
+   for(int i=0;i<NUMPIXELS;i++)
+   {
+    pixels.setPixelColor(i, pixels.Color(0,255,0));   // Moderately bright green color.
+    pixels.show();    // This sends the updated pixel color to the hardware.
+   }
+   delay(1000);  // Delay for a period of time (in milliseconds).
+   
+   for(int i=0;i<NUMPIXELS;i++)
+   {
+    pixels.setPixelColor(i, pixels.Color(0,0,255));   // Moderately bright green color.
+    pixels.show();    // This sends the updated pixel color to the hardware.
+   }
+   delay(1000);  // Delay for a period of time (in milliseconds).
 }
