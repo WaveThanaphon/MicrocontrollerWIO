c6f2c2c45bd55478ad7c63dbff6dfe6dab07ce0f
Nathee Srina <thanaphon.wich@lic.ac.th>
2024-11-08 11:54:19 +0700

    Update NeoPixcel.ino

--- NeoPixcel/NeoPixcel.ino
+++ NeoPixcel/NeoPixcel.ino
@@ -7,43 +7,12 @@ void setup()
   pixels.begin(); // สั่งให้เริ่มต้นการทำงาน
   pixels.setBrightness(10); // สั่งให้ความสว่างเป็น 10%
 }
-// void loop() 
-// {
-//    for(int i=0;i<NUMPIXELS;i++) // วนลูปเปลี่ยนสีของ LED
-//    {
-//       pixels.setPixelColor(i, pixels.Color(255,0,0));   // แสดงสีแดง
-//       pixels.show();    // สั่งให้แสดงสีที่เลือก
-//    }
-//    delay(1000);  
 
-//    for(int i=0;i<NUMPIXELS;i++)
-//    {
-//     pixels.setPixelColor(i, pixels.Color(0,255,0));   // Moderately bright green color.
-//     pixels.show();    // This sends the updated pixel color to the hardware.
-//    }
-//    delay(1000);  // Delay for a period of time (in milliseconds).
-   
-//    for(int i=0;i<NUMPIXELS;i++)
-//    {
-//     pixels.setPixelColor(i, pixels.Color(0,0,255));   // Moderately bright green color.
-//     pixels.show();    // This sends the updated pixel color to the hardware.
-//    }
-//    delay(1000);  // Delay for a period of time (in milliseconds).
-// }
-unsigned long previousMillis = 0;
-const long interval = 1000;
 int currentColor = 0;
 
 void loop() 
 {
-   unsigned long currentMillis = millis();
-
-   if (currentMillis - previousMillis >= interval) 
-   {
-      previousMillis = currentMillis;
-
-      for(int i=0; i<NUMPIXELS; i++) 
-      {
+   for(int i=0; i<NUMPIXELS; i++){
          if (currentColor == 0) 
          {
             pixels.setPixelColor(i, pixels.Color(255,0,0)); // Red
@@ -56,13 +25,12 @@ void loop()
          {
             pixels.setPixelColor(i, pixels.Color(0,0,255)); // Blue
          }
-      }
-      pixels.show();
-
-      currentColor++;
-      if (currentColor > 2) 
-      {
-         currentColor = 0;
-      }
+   }
+   pixels.show();
+   delay(1000);
+   currentColor++;
+   if (currentColor > 2) 
+   {
+      currentColor = 0;
    }
 }
