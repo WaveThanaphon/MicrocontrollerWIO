95d96a0f01d7db39ff6fc36ca66f6c35b18a054b
Nathee Srina <nathee.sri@lic.ac.th>
2024-11-08 11:35:42 +0700

    Update NeoPixcel.ino

--- NeoPixcel/NeoPixcel.ino
+++ NeoPixcel/NeoPixcel.ino
@@ -1,34 +1,28 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN            BCM27
-#define NUMPIXELS      12
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int delayval = 500; // delay for half a second
+#define PIN            0 // กำหนดขาที่ต่อกับ NeoPixel
+#define NUMPIXELS      12 // กำหนดจำนวน LED ที่ต่อกับ NeoPixel
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // สร้างอ็อบเจ็ค pixels
 void setup() 
 {
-  pixels.begin(); // This initializes the NeoPixel library.
-  pixels.setBrightness(10);
-  pixels.show();
+  pixels.begin(); // สั่งให้เริ่มต้นการทำงาน
+  pixels.setBrightness(10); // สั่งให้ความสว่างเป็น 10%
 }
 void loop() 
 {
-   for(int i=0;i<NUMPIXELS;i++)
-   {
-      pixels.setPixelColor(i, pixels.Color(255,0,0));   // Moderately bright green color.
-      pixels.show();    // This sends the updated pixel color to the hardware.
-   }
-   delay(1000);  // Delay for a period of time (in milliseconds).
+   // for(int i=0;i<NUMPIXELS;i++) // วนลูปเปลี่ยนสีของ LED
+   // {
+   //    pixels.setPixelColor(i, pixels.Color(255,0,0));   // แสดงสีแดง
+   //    pixels.show();    // สั่งให้แสดงสีที่เลือก
+   // }
+   // delay(1000); 
+   static unsigned long lastUpdate = 0;
+   unsigned long currentMillis = millis();
 
-   for(int i=0;i<NUMPIXELS;i++)
-   {
-    pixels.setPixelColor(i, pixels.Color(0,255,0));   // Moderately bright green color.
-    pixels.show();    // This sends the updated pixel color to the hardware.
+   if (currentMillis - lastUpdate >= 1000) {
+      for(int i=0;i<NUMPIXELS;i++) {
+         pixels.setPixelColor(i, pixels.Color(0,255,0));   // แสดงสีเขียว
+         pixels.show();    // สั่งให้แสดงสีที่เลือก
+      }
+      lastUpdate = currentMillis;
    }
-   delay(1000);  // Delay for a period of time (in milliseconds).
-   
-   for(int i=0;i<NUMPIXELS;i++)
-   {
-    pixels.setPixelColor(i, pixels.Color(0,0,255));   // Moderately bright green color.
-    pixels.show();    // This sends the updated pixel color to the hardware.
-   }
-   delay(1000);  // Delay for a period of time (in milliseconds).
 }
